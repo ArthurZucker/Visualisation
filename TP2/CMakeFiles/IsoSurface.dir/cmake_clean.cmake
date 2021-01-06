76528487799561b8b63997714fb50af15b2c8108
ArthurZucker <arthur.zucker@gmail.com>
2021-01-06 08:26:53 +0100

    added files

---
+++ TP2/CMakeFiles/IsoSurface.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IsoSurface.dir/main.cpp.o"
+  "IsoSurface"
+  "IsoSurface.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IsoSurface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TP2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vtkPoints.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	cout<<"Congrats"<<endl;
+	return 0;
+}
